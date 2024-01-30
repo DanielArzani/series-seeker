7da1567d9edf69a2a506f7b23460fd702ecb523d
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2024-01-30 03:22:55 -0500

    fix search bar styles

--- app/components/SearchBar/SearchBar.tsx
+++ app/components/SearchBar/SearchBar.tsx
@@ -10,27 +10,21 @@ import searchIcon from '@/public/icon-search.svg';
  */
 export default function SearchBar() {
   return (
-    <div role='search'>
-      <label className='search-label' htmlFor='search'>
-        <span className='sr-only'>Search for any movies or TV series</span>
-        <p id='error' role='alert'></p>
+    <div role='search' className='relative w-full'>
+      <label htmlFor='search' className='sr-only'>
+        Search for any movies or TV series
       </label>
+      <p id='error' role='alert' className='sr-only'></p>
 
-      <div>
-        <div className='relative'>
-          <input
-            id='search'
-            className='bg-inherit border-transparent w-full'
-            type='text'
-            placeholder='Search for movies or TV series'
-          />
-          <Image
-            src={searchIcon}
-            alt=''
-            width={20}
-            height={20}
-            className='absolute top-1/2 -left-6 -translate-y-1/2'
-          />
+      <div className='flex items-center'>
+        <input
+          id='search'
+          className='bg-inherit border-transparent pl-10 w-full'
+          type='text'
+          placeholder='Search for movies or TV series'
+        />
+        <div className='absolute left-0 inset-y-0 flex items-center pl-3 pointer-events-none'>
+          <Image src={searchIcon} alt='Search' width={20} height={20} />
         </div>
       </div>
     </div>
