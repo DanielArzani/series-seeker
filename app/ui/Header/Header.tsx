9c7605d8ad9141859f12b13c635f339a35573c77
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2024-01-30 03:09:39 -0500

    fix layout for header

--- app/ui/Header/Header.tsx
+++ app/ui/Header/Header.tsx
@@ -8,7 +8,7 @@ import Navbar from '../Navbar/Navbar';
 export default function Header() {
   return (
     <>
-      <header className='header flex w-full max-w-screen-2xl justify-between items-center bg-headerBg px-4 py-4 lg:left-0 lg:flex-col lg:w-auto md:rounded-[10px] '>
+      <header className='header flex justify-between px-4 lg:px-0 lg:py-9 items-center bg-headerBg lg:left-0 lg:flex-col lg:w-auto md:rounded-[10px] '>
         <Logo />
 
         <Navbar />
