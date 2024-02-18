0569b7928e4fcd66bfc414480aea833b5d0ff87c
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2024-02-18 01:29:51 -0500

    change client side fetch calls to server side and add a simple, manual caching strategy to save on api requests

--- app/lib/api/getChannel.ts
+++ app/lib/api/getChannel.ts
@@ -5,19 +5,30 @@ import {
 
 const EXAMPLE_CHANNEL_ID = 'UCzpl6CJP6lo5vjsEAeIHnsg';
 
+// TODO: Use a proper cache library, this is only for saving on api requests during development
+let cache: { [key: string]: YouTubeChannel | null } = {};
+
 /**
  * Retrieves a YouTube channel's details by ID. Returns null if channel is not found or on error.
  * @param channelId - The YouTube channel's unique identifier.
  */
 export async function getChannel(
-  channelId: string
+  channelId: string,
+  bypassCache: boolean = false
 ): Promise<YouTubeChannel | null> {
+  // If the channel is in the cache and bypassCache is not true, return it
+  if (!bypassCache && cache[channelId]) {
+    console.log(`Serving ${channelId} from cache`);
+    return cache[channelId];
+  }
+
   const API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
 
   const url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${channelId}&key=${API_KEY}`;
 
   try {
     const response = await fetch(url);
+
     if (!response.ok) {
       console.error(
         `API Request Failed: ${response.status} ${response.statusText}`
@@ -28,8 +39,12 @@ export async function getChannel(
     const data: YouTubeChannelListResponse = await response.json();
     if (data.items.length === 0) return null;
 
+    // Store the channel in the cache
+    cache[channelId] = data.items[0];
+    console.log(`Storing ${channelId} in cache`);
+
     // Since we are fetching details for a single channel, return the first item
-    console.log(data.items[0]);
+    // console.log(data.items[0]);
     return data.items[0];
   } catch (error) {
     console.error('Error fetching channel details:', error);
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,33 @@
-'use client';
 import MainContent from './ui/MainContent/MainContent';
 import TrendingContent from './ui/TrendingContent/TrendingContent';
 
-import ContentHolder from './ui/ContentHolder/ContentHolder';
-import { ChannelProvider } from './contexts/channelContext';
+import { getChannel } from './lib/api/getChannel';
+import Image from 'next/image';
 
 /**
  * The homepage
  * The root route (/)
  */
-export default function Homepage() {
+export default async function Homepage() {
+  // const channel = await getChannel('UCzpl6CJP6lo5vjsEAeIHnsg');
+  const channel = await getChannel('UCrcrDXK620kopfSffPxrdTA');
+
+  if (!channel) {
+    return <h1>Channel not found</h1>;
+  }
+
   return (
     <>
-      <ChannelProvider>
-        <ContentHolder />
-      </ChannelProvider>
-      {/* <TrendingContent />
-      <MainContent /> */}
+      <div>
+        <h2>{channel.snippet.title}</h2>
+        <p>{channel.snippet.description}</p>
+        <Image
+          src={`${channel.snippet.thumbnails.default.url}`}
+          alt='thumbnail'
+          width={`${channel.snippet.thumbnails.default.width}`}
+          height={`${channel.snippet.thumbnails.default.height}`}
+        />
+      </div>
     </>
   );
 }
