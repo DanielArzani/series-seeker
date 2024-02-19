20939b0b06d176baf2c9f633043700239839d9f1
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2024-02-19 02:43:39 -0500

    add youtube env variable to vercel

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,7 @@ import MainContent from './ui/MainContent/MainContent';
 import TrustedChannels from './ui/TrustedChannels/TrustedChannels';
 
 /**
- * The homepage
- * The root route (/). Displays my recommendations for both youtube channels and videos
+ * Displays my recommendations for both youtube channels and videos
  */
 export default function Homepage() {
   return (
