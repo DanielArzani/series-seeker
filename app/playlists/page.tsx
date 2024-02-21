566af42639ccce8368fc92254049214b7b271350
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2024-02-21 03:09:27 -0500

    attempt to use route handlers instead of client side fetching:

--- app/playlists/page.tsx
+++ app/playlists/page.tsx
@@ -9,55 +9,77 @@ import Image from 'next/image';
 /**
  * Finds playlists that match the users search query
  */
-export default function PlaylistsPage() {
-  // const searchQuery = useSearchParams();
-  // const [playlists, setPlaylists] = useState<PlaylistType[] | null>(null);
+export default function PlaylistPage() {
+  {
+    const searchQuery = useSearchParams();
+    const [playlists, setPlaylists] = useState<PlaylistType[] | null>(null);
 
-  // useEffect(() => {
-  //   const query = searchQuery.get('query');
+    // useEffect(() => {
+    //   const query = searchQuery.get('query');
 
-  //   if (query === null) return;
+    //   if (query === null) return;
 
-  //   (async () => {
-  //     const data = await getPlaylists(query);
-  //     setPlaylists(data);
-  //   })();
-  // }, [searchQuery]);
+    //   (async () => {
+    //     const data = await getPlaylists(query);
+    //     setPlaylists(data);
+    //   })();
+    // }, [searchQuery]);
 
-  // if (playlists === null) return <div className='text-white playlists'></div>;
+    //! This attempt at using route handlers to fetch data was unsuccessful
+    // useEffect(() => {
+    //   const query = searchQuery.get('query');
+    //   if (!query) return;
 
-  // if (playlists.length === 0)
-  //   return <div className='text-white'>No playlists found</div>;
+    //   const fetchData = async () => {
+    //     const response = await fetch(`/api/playlists`);
+    //     if (response.ok) {
+    //       const data = await response.json();
+    //       setPlaylists(data);
+    //     } else {
+    //       // Handle errors or set an error state as needed
+    //       console.error('Failed to fetch playlists');
+    //     }
+    //   };
 
-  //LINK: https://www.youtube.com/playlist?list=PLUH8bFeIEPCHzVYaD_W3CY0cyMJUZ_Js0
-  return (
-    <>
-      <div className='text-white playlists'>
-        <p>Chill Beach - Surf Music</p>
+    //   fetchData();
+    // }, [searchQuery]);
 
-        <p>Violeta Bocage</p>
+    if (playlists === null) return <div className='text-white playlists'></div>;
 
-        <Image
-          src={
-            'https://series-seeker.vercel.app/_next/image?url=https%3A%2F%2Fi.ytimg.com%2Fvi%2FoRdbvGgPPiw%2Fhqdefault.jpg&w=1080&q=75'
-          }
-          alt=''
-          width={480}
-          height={360}
-        />
-      </div>
+    if (playlists.length === 0)
+      return <div className='text-white'>No playlists found</div>;
 
-      {/* <div className='text-white playlists'>
-        <p>{playlists[0].snippet.title}</p>
-        <p>{playlists[0].snippet.description}</p>
-        <p>{playlists[0].snippet.channelTitle}</p>
-        <Image
-          src={playlists[0].snippet.thumbnails.high.url}
-          width={playlists[0].snippet.thumbnails.high.width}
-          height={playlists[0].snippet.thumbnails.high.height}
-          alt=''
-        />
-      </div> */}
-    </>
-  );
+    //PLAYLIST URL EXAMPLE: https://www.youtube.com/playlist?list=PLUH8bFeIEPCHzVYaD_W3CY0cyMJUZ_Js0
+
+    // Direct URL to the image
+    const imageUrl = 'https://i.ytimg.com/vi/oRdbvGgPPiw/hqdefault.jpg';
+    return (
+      <>
+        {/* place holder so that I can style without making more api requests */}
+        {/* <div className='text-white playlists'>
+          <p>Chill Beach - Surf Music</p>
+
+          <p>Violeta Bocage</p>
+
+          <Image src={imageUrl} alt='' width={480} height={360} />
+        </div> */}
+
+        <div className='text-white playlists'>
+          {playlists.map((playlist, index) => (
+            <div key={index}>
+              <p>{playlist.snippet.title}</p>
+              <p>{playlist.snippet.description}</p>
+              <p>{playlist.snippet.channelTitle}</p>
+              <Image
+                src={playlist.snippet.thumbnails.high.url}
+                width={playlist.snippet.thumbnails.high.width}
+                height={playlist.snippet.thumbnails.high.height}
+                alt={playlist.snippet.title}
+              />
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  }
 }
